3291ac7d49cd35cf1d68aaf7ce7b6f3233f8bd0b
Oink <naveenkumar04122004@gmail.com>
2026-05-20 13:00:17 +0530

    Refactor cart loading logic and add unit tests for addToCart and formatCurrency functions

---
+++ tests-jasmine/data/cartTest.js
@@ -0,0 +1,51 @@
+import { 
+  addToCart, 
+  cart, 
+  loadFromStorage 
+} from '../../data/cart.js';
+
+//flakyTest-test somethime pass or fails
+//unit test
+//we have to do complex test an integration test meaning test many units or pices of code working together
+describe('test suite: addToCart', () => {
+  it('add existing product to the cart', () => {
+
+    spyOn(localStorage, 'setItem');
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
+    });
+
+    loadFromStorage();
+
+    addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(2);
+    expect(cart[0].deliveryOptionId).toEqual('1');
+
+  });
+
+  it('add the new product to the cart', () => {
+
+    spyOn(localStorage, 'setItem')
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([]);
+    });
+
+    loadFromStorage();
+
+    addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(1);
+    expect(cart[0].deliveryOptionId).toEqual('1');
+
+  });
+  
+});
