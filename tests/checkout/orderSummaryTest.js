2cba41e62e18644bd7a7de2e2b90443a8ed17f40
Oink <naveenkumar04122004@gmail.com>
2026-05-24 15:08:38 +0530

    Enhance renderOrderSummary function with dynamic class names for product details; update related tests for improved accuracy and coverage

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -36,19 +36,33 @@ describe('test suite: renderOrderSummary', () => {
 
   it('displays the cart', () => {
 
-  expect(
-    document.querySelectorAll('.js-cart-item-container').length
-  ).toEqual(2);
+    expect(
+      document.querySelectorAll('.js-cart-item-container').length
+    ).toEqual(2);
 
-  expect(
-    document.querySelector(`.js-product-quantity-${productId1}`).innerText
-  ).toContain('Quantity: 2');
-  
-  expect(
-    document.querySelector(`.js-product-quantity-${productId2}`).innerText
-  ).toContain('Quantity: 1');
+    expect(
+      document.querySelector(`.js-product-name-${productId1}`).innerText
+    ).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
 
-  document.querySelector('.js-test-container').innerHTML = ``;
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toEqual('Intermediate Size Basketball');
+
+    expect(
+      document.querySelector(`.js-product-quantity-${productId1}`).innerText
+    ).toContain('Quantity: 2');
+    
+    expect(
+      document.querySelector(`.js-product-quantity-${productId2}`).innerText
+    ).toContain('Quantity: 1');
+
+    expect(
+      document.querySelector(`.js-product-price-${productId1}`).innerText
+    ).toEqual('$10.90');
+
+    expect(
+      document.querySelector(`.js-product-price-${productId2}`).innerText
+    ).toEqual('$20.95');
 
   });
 
@@ -68,6 +82,14 @@ describe('test suite: renderOrderSummary', () => {
       document.querySelectorAll('.js-cart-item-container').length
     ).toEqual(1);
 
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toEqual('Intermediate Size Basketball');
+
+    expect(
+      document.querySelector(`.js-product-price-${productId2}`).innerText
+    ).toEqual('$20.95');
+
     expect (
       document.querySelector(`.js-cart-item-container-${productId1}`)
     ).toEqual(null);
@@ -78,9 +100,11 @@ describe('test suite: renderOrderSummary', () => {
 
     expect (cart.length).toEqual(1);
     expect (cart[0].productId).toEqual(productId2);
+    
+  });
 
+  afterEach(() => {
     document.querySelector('.js-test-container').innerHTML = ``;
-
   });
 
 });
