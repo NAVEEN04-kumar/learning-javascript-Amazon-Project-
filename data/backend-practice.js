92a6c6bb490130620a09780c336d325e652b6b3e
Oink <naveenkumar04122004@gmail.com>
2026-05-29 17:12:08 +0530

    Add backend Practice file

---
+++ data/backend-practice.js
@@ -0,0 +1,25 @@
+//user send request in the form of HTTP message
+//backend computer recieve the request from the user and process it 
+/* Type of HTTP Method
+
+  1,GET - get some information from the backend.
+  2.POST
+  3.PUT
+  4.DELETE
+
+  url-uniform resource locater
+  https://amazon.com - domain name
+    |        
+  means use http to communicate with the domain  
+*/
+
+const xhr =  new XMLHttpRequest();
+
+xhr.addEventListener('load', () => {
+  console.log(xhr.response);
+});
+
+console.log(xhr);
+
+xhr.open('GET', 'https://supersimplebackend.dev');
+xhr.send(); 
