13a163696e912d52897e62ee500a5b0bf12790d9
Oink <naveenkumar04122004@gmail.com>
2026-06-02 11:09:59 +0530

    convert loadProducts from xhr to fetch

--- data/products.js
+++ data/products.js
@@ -110,6 +110,39 @@ object3.method();
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map( productDetails => {
+      if(productDetails.type) {
+
+        if(productDetails.type == "clothing") {
+          return new Clothing(productDetails);
+        }
+
+        if(productDetails.type == "appliance") {
+          return new Appliance(productDetails);
+        }
+      }
+    
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+
+  });
+
+  return promise;
+}
+
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+
+/*
 export function loadProducts(fun) {
 
   const xhr = new XMLHttpRequest();
@@ -138,6 +171,7 @@ export function loadProducts(fun) {
   xhr.send();
 
 }
+*/
 
 /*
   export const products = [
--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,6 +1,6 @@
 import { cart } from '../../data/cart-class.js';
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
-import { loadProducts } from '../../data/products.js';
+import { loadProductsFetch } from '../../data/products.js';
 
 //integration test: how the page looks and how the page behaves
 //Hooks: Let us run some code for each test
@@ -10,7 +10,7 @@ describe('test suite: renderOrderSummary', () => {
   const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
 
   beforeAll((done) => {
-    loadProducts(() => {
+    loadProductsFetch().then(() => {
       done();
     });
   });
