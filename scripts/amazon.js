fc960474a89685d2fc396bb334f0383558d146cf
Oink <naveenkumar04122004@gmail.com>
2026-06-10 12:37:03 +0530

    made addedToCart appear on screen and disapper after 2 second using classList and set TImeout

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -52,7 +52,7 @@ function renderPRoductsGrid() {
 
         <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-added-to-cart-${product.id}">
             <img src="images/icons/checkmark.png">
               Added
         </div>
@@ -84,8 +84,30 @@ function renderPRoductsGrid() {
       const productId = button.dataset.productId;
       cart.addToCart(productId);
       updateCartQuantity();
+      addedToCartCheckmark(productId);
     });
   });
+
+  const addedMessageTimeouts = {}
+
+  function addedToCartCheckmark(productId) {
+    const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+
+    addedMessage.classList.add('added-to-cart-visible');
+    const previousTimeoutId = addedMessageTimeouts[productId];
+
+    if (previousTimeoutId) {
+      clearTimeout(previousTimeoutId);
+    }
+
+    const timeoutId = setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000);
+
+    
+   addedMessageTimeouts[productId] = timeoutId; 
+  }
+
 }
 
 loadProductsFetch().then(() => {
